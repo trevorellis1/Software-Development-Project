ca2da59539ad8dc1020adf05da86d30386eb2cfe
trevorellis1 <57125808+trevorellis1@users.noreply.github.com>
2019-12-04 09:28:07 -0500

    Add files via upload

---
+++ DragandDrop/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private void listBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                listBox1.Items.Add(s[i]);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.listBox1.DragDrop += new
+           System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new
+                       System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+        }
+
+
+    }
+    
+  
+}
